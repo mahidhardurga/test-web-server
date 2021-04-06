1fe1e31c5d0baad9ee5ef758df3aede969fa7b11
850694 <850694@LTIN270752.cts.com>
2021-04-06 22:04:55 +0530

    final try

--- demoPractice/src/main/java/com/practice/controller/demoPracticeController.java
+++ demoPractice/src/main/java/com/practice/controller/demoPracticeController.java
@@ -5,27 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-//@RequestMapping("/demoPractice")
 public class demoPracticeController {
 	
 	@RequestMapping("/Home")
 	public String home() {
 		return "home";
 	}
-	
-	/*@RequestMapping("/addStudent")
-	public String addStudent(@ModelAttribute("student") Student student, Model model) {
-		service.save(student);
-		model.addAttribute("isDone", true);
-		model.addAttribute("student", student);
-		model.addAttribute("Sucess", "student created sucessfully");
-		return "home";
-	}*/
-	
-	@RequestMapping("/shoes")
-	public String shoes() {
-		return "home";
-	}
-	
 	
 }
